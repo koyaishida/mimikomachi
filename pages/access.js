ec54634a5ac762603b629f97da441a7ea31fd789
石田航也 <koyaishida@ishidawarinoMBP.local>
2020-09-09 01:26:44 +0900

    fixed regularMap

--- pages/access.js
+++ pages/access.js
@@ -33,7 +33,8 @@ export default function Access() {
         <p>日野保育園のすぐ近くです！</p>
         <div className={Style.map}>
           <RegularMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}`}
+            // googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}`}
+            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_API_KEY}`}
             loadingElement={<div style={{ height: "100%" }} />}
             containerElement={<div style={{ height: "400px" }} />}
             mapElement={<div style={{ height: "100%" }} />}
