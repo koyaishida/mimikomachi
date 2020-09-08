c6107b21afc14de786e10617c2bfb57f3a61a65e
石田航也 <koyaishida@ishidawatarunarinoMacBook-Pro.local>
2020-09-08 20:41:08 +0900

    fixed APIKEY

--- pages/access.js
+++ pages/access.js
@@ -9,7 +9,7 @@ import {
 } from "react-google-maps";
 
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-console.log(API_KEY, "API");
+console.log(API_KEY, "API!");
 
 const RegularMap = withScriptjs(
   withGoogleMap(() => (
